4055171c441ef08ec95562e05d5c848533606a61
Ktjj91 <kingsleythomas33@gmail.com>
2024-08-30 00:28:46 +0200

    deuxieme commit

--- Main.java
+++ Main.java
@@ -3,9 +3,16 @@
  */
 public class Main {
   public static void main(String[] args) {
-    
+
     System.out.println("Hello world !");
+
+    //chec if number than is upper 10
+    boolean isUpperThan10 = isUpperThen(11);
+    System.out.println(isUpperThan10);
   }
 
-  
+  public static boolean isUpperThen(int number) {
+    return number > 10;
+  }
+
 }
